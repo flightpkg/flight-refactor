165face9e39df4aa26376800e96da6d97ffbba9e
grng <36968271+monitrr@users.noreply.github.com>
2022-11-20 10:26:09 +0000

    feat/updating
    * add version
    
    * feat/updating easter egg
    
    * add update logic for Windows (not finished)
    
    * figlet fix-ish
    
    * remove figlet entirely
    
    * add dists
    
    * remove figlet again
    
    * hope this works

--- src/func/func.go
+++ src/func/func.go
@@ -1,16 +1,21 @@
 package flightpkg
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 
 	grab "github.com/cavaliergopher/grab/v3"
 	targz "github.com/walle/targz"
 )
+
+// var version string = "2.0.3"
+var version string = "0.0.0"
 
 func Install(args []string) {
 	registry := "https://registry.yarnpkg.com/"
@@ -59,20 +64,99 @@ func Uninstall(args []string) {
 		fmt.Println("flight uninstall <pkg>")
 	} else {
 		os.RemoveAll(fmt.Sprintf("./node_modules/%v", args[0]))
+		fmt.Println("Uninstalled " + args[0])
+
+		files, _ := ioutil.ReadDir("./node_modules")
+		if len(files) == 0 {
+			os.Remove("./node_modules")
+		}
 	}
 }
 
-func Figlet() {
-	// Open the file.
-	f, _ := os.Open("..\\misc\\flight.txt")
-	// Create a new Scanner for the file.
-	scanner := bufio.NewScanner(f)
-	// Loop over all lines in the file and print them.
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+func Status() {
+	version_dotless, _ := strconv.ParseInt(strings.ReplaceAll(version, ".", ""), 10, 64)
+	latest := "https://api.github.com/repos/flightpkg/flight-v2/releases/latest"
+	fmt.Printf("Version: %v\n", version)
+	resp, err := http.Get(latest)
+	if err != nil {
+		fmt.Println(err)
 	}
-	fmt.Println()
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var data map[string]interface{}
+
+	/* Getting the version of the package that is being installed. */
+	json.Unmarshal(body, &data)
+
+	latest_tag := data["tag_name"].(string)
+	latest_tag_dotless, _ := strconv.ParseInt(strings.Replace(latest_tag, ".", "", -1), 10, 64)
+
+	if int(version_dotless) < int(latest_tag_dotless) {
+		fmt.Printf("Update Available:\n %v -> %v", version, latest_tag)
+	} else if int(version_dotless) > int(latest_tag_dotless) {
+		fmt.Println("You're a time traveler, aren't you?")
+	} else {
+		fmt.Println("Up to date!")
+	}
+}
+
+func Update() {
+	latest := "https://api.github.com/repos/flightpkg/flight-v2/releases/latest"
+	resp, err := http.Get(latest)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var data map[string]interface{}
+
+	/* Getting the version of the package that is being installed. */
+	json.Unmarshal(body, &data)
+	latest_tag := data["tag_name"].(string)
+	latest_tag_dotless, _ := strconv.ParseInt(strings.Replace(latest_tag, ".", "", -1), 10, 64)
+	version_dotless, _ := strconv.ParseInt(strings.ReplaceAll(version, ".", ""), 10, 64)
+
+	fmt.Printf("%v -> %v\n", version, latest_tag)
+
+	op := runtime.GOOS
+	switch op {
+	case "windows":
+		if int(version_dotless) < int(latest_tag_dotless) {
+			apd, _ := os.UserConfigDir()
+			url := data["assets"].([]interface{})[0].(map[string]interface{})["browser_download_url"].(string)
+			os.Mkdir(apd+"/.flightpkg/bin", 0777)
+			os.Remove(fmt.Sprintf("%v/.flightpkg/bin/flight.exe", apd))
+			_, err := grab.Get(fmt.Sprintf("%v/.flightpkg/bin/flight.exe", apd), url)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Updated to " + latest_tag)
+		} else {
+			fmt.Println("Up to date!")
+		}
+	case "darwin":
+		//TODO: Add MacOS support
+	case "linux":
+		//TODO: Add Linux support
+	default:
+		fmt.Printf("%s.\n", op)
+	}
+
 }
 
 func Help() {
@@ -82,5 +166,6 @@ flight help
 flight version
 flight install <pkg>
 flight uninstall <pkg>
-flight update`)
+flight update
+flight status`)
 }
--- src/main.go
+++ src/main.go
@@ -8,16 +8,19 @@ import (
 
 func main() {
 	cli := os.Args[1:]
-	funcs.Figlet()
 	if len(cli) == 0 {
 		fmt.Println("flight <command> [arguments]")
 	} else {
-		if cli[0] == "-h" {
+		if cli[0] == "-h" || cli[0] == "--help" {
 			funcs.Help()
 		} else if cli[0] == "install" || cli[0] == "i" || cli[0] == "add" || cli[0] == "a" || cli[0] == "get" || cli[0] == "g" {
 			funcs.Install(cli[1:])
 		} else if cli[0] == "uninstall" || cli[0] == "u" || cli[0] == "remove" || cli[0] == "r" || cli[0] == "ui" {
 			funcs.Uninstall(cli[1:])
+		} else if cli[0] == "status" || cli[0] == "s" || cli[0] == "st" {
+			funcs.Status()
+		} else if cli[0] == "update" || cli[0] == "up" || cli[0] == "u" {
+			funcs.Update()
 		} else {
 			fmt.Println("flight <command> [arguments]")
 		}
